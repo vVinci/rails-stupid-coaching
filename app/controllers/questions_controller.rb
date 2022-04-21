d915765c90c1c3e3d38aad38f62a2f4a4fcb1da8
Vinicius Vinci <vvinci@protonmail.com>
2022-04-21 20:13:16 -0300

    code is working but still ugly

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,18 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:ask]
+    @answer = if @question.end_with?('?')
+                'Silly question, get dressed and go to work!'
+
+              elsif @question == 'I am going to work right now!'
+                'Great!'
+
+              elsif @question.instance_of?(String)
+                "I don't care, get dressed and go to work!"
+
+              else
+                'exit'
+              end
   end
 end
